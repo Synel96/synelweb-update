0f9a7f0346e677c287ea3b7609c1ac0ec3b530ce
Szilveszter <nmszilveszter@gmail.com>
2026-06-27 12:00:54 +0200

    Remove Vercel-specific config and plugin wiring

--- +server.ts
+++
@@ -1,9 +0,0 @@
-import { importServerProductionEntry } from "@brillout/vite-plugin-server-entry/runtime";
-
-if (process.env.NODE_ENV === "production") {
-  await importServerProductionEntry();
-}
-
-export default {
-  prod: {},
-};
